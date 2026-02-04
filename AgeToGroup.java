61b366a3994a6b6448ef3b47fb091fb5ee3b5c8a
YoussefAtGit <yb316@exeter.ac.uk>
2026-02-04 14:18:15 +0000

    PrintSquares completed with for loop

--- AgeToGroup.java
+++ AgeToGroup.java
@@ -1,8 +1,8 @@
 public class AgeToGroup {
     public static void main(String[] args) {
         int age = Integer.parseInt(args[0]);
-
-        int ageGroup = (age<=18?) 0 : age>=65 ? 2:1;      
+ 
+        int ageGroup = (age<=18) ? 0 : (age>=65) ? 2:1;      
             
         System.out.println("Age: " + age + ", Age Group: " + ageGroup);
     }
